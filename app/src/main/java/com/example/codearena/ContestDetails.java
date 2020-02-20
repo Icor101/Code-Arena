8d2d1c8eac46bb09e5ef7c9552465cab6a81b958
Riya <riyamish16@gmail.com>
2020-02-20 22:28:14 +0530

    Implemented RecyclerView with dummy data

---
+++ app/src/main/java/com/example/codearena/ContestDetails.java
@@ -0,0 +1,15 @@
+package com.example.codearena;
+
+class ContestDetails {
+    String start_time;
+    String contest_title;
+    String platform;
+    String duration;
+
+    ContestDetails(String start_time, String contest_title, String platform, String duration) {
+        this.start_time = start_time;
+        this.contest_title = contest_title;
+        this.platform = platform;
+        this.duration = duration;
+    }
+}
---
+++ app/src/main/java/com/example/codearena/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.codearena;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<ContestDetails> myDataset = new ArrayList<>();
+        myDataset.add(new ContestDetails("9:30PM", "CookOff", "CodeChef", "2.5 hrs"));
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
