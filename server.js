fe177d2ead4493181726ce50d957da5937b6eec4
Riya <riyamish16@gmail.com>
2020-02-21 13:41:08 +0530

    Scraping all running contests

--- server.js
+++ server.js
@@ -7,22 +7,24 @@ var url='https://clist.by/'
 function scrape(body){
 	
 	const dom = parser.parse(body);
-	var nodeList = dom.querySelectorAll('.row contest running');
+	var nodeList = dom.querySelectorAll('.row.contest.running');
+	var dataSet = [];
 	
-	console.log(nodeList);
+	//console.log(nodeList);
 	
 	for(var i=0;i<nodeList.length;i++){
 		
 		var json = {start_time:"",contest_title:"",platform:"",duration:""};
 		
-		json.start_time = nodeList[i].querySelector('.col-md-5 col-sm-12 start-time').text;
-		json.duration = nodeList[i].querySelector('.col-md-3 col-sm-6 duration').text;
-		json.contest_title = nodeList[i].querySelector('.contest_title').querySelector('a').text;
-		json.platform = nodeList[i].querySelector('.text-muted').text;
+		json.start_time = nodeList[i].querySelector('.col-md-5.col-sm-12.start-time').text.trim();
+		json.duration = nodeList[i].querySelector('.col-md-3.col-sm-6.duration').text.trim();
+		json.contest_title = nodeList[i].querySelector('.contest_title').querySelector('a').text.trim();
+		json.platform = nodeList[i].querySelector('.text-muted').text.trim();
 		
-		console.log("HELLO"+JSON.stringify(json));
+		dataSet.push(json);
+		//console.log(JSON.stringify(json));
 	}
-	
+	console.log(dataSet);	
 }
 
 fetch(url).then(res => res.text()).then(body => scrape(body));
