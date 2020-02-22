96d514e96f81a224c668868c274dcf7783fb9361
Riya <riyamish16@gmail.com>
2020-02-22 18:01:49 +0530

    updated web service. Added scraping for past & upcoming contests.

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const parser = require('node-html-parser');
 
 var url = 'https://clist.by/'
 var express = require("express")
-//var routes = require("./routes/routes.js")
+
 var app = express();
-var dataSet = [];
+var live = [], upcoming = [], past = [];
 
 function scrape(body){
 	
 	const dom = parser.parse(body);
 	var nodeList = dom.querySelectorAll('.row.contest.running');
-	
-	//console.log(nodeList);
 	
 	for(var i=0;i<nodeList.length;i++){
 		
@@ -24,21 +22,55 @@ function scrape(body){
 		json.contest_title = nodeList[i].querySelector('.contest_title').querySelector('a').text.trim();
 		json.platform = nodeList[i].querySelector('.text-muted').text.trim();
 		
-		dataSet.push(json);
-		//console.log(JSON.stringify(json));
+		live.push(json);
 	}
-	console.log(dataSet);	
+
+	nodeList = dom.querySelectorAll('.row.contest.past');
+
+	for(var i=0;i<nodeList.length;i++){
+		
+		var json = {start_time:"",contest_title:"",platform:"",duration:""};
+		
+		json.start_time = nodeList[i].querySelector('.col-md-5.col-sm-12.start-time').text.trim();
+		json.duration = nodeList[i].querySelector('.col-md-3.col-sm-6.duration').text.trim();
+		json.contest_title = nodeList[i].querySelector('.contest_title').querySelector('a').text.trim();
+		json.platform = nodeList[i].querySelector('.text-muted').text.trim();
+		
+		past.push(json);
+	}
+
+	nodeList = dom.querySelectorAll('.row.contest.coming');
+
+	for(var i=0;i<nodeList.length;i++){
+		
+		var json = {start_time:"",contest_title:"",platform:"",duration:""};
+		
+		json.start_time = nodeList[i].querySelector('.col-md-5.col-sm-12.start-time').text.trim();
+		json.duration = nodeList[i].querySelector('.col-md-3.col-sm-6.duration').text.trim();
+		json.contest_title = nodeList[i].querySelector('.contest_title').querySelector('a').text.trim();
+		json.platform = nodeList[i].querySelector('.text-muted').text.trim();
+		
+		upcoming.push(json);
+	}
 }
 
 fetch(url).then(res => res.text()).then(body => scrape(body));
 
-app.get('/',(req,res) => {
-	res.send(dataSet);
+app.get('/live',(req,res) => {
+	res.send(live);
 });
+
+app.get('/past',(req,res) => {
+	res.send(past);
+});
+
+app.get('/upcoming',(req,res) => {
+	res.send(upcoming);
+});
+
 
 var server = app.listen(3000, () =>{
 	console.log('Listening to port 3000...');	
 });
 
-//module.exports(dataSet);
 
