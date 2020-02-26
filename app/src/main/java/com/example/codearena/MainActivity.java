e8260bd5cf08b3eb731e55572f3eed2020047e9f
Riya <riyamish16@gmail.com>
2020-02-26 11:10:20 +0530

    Added recycler view and web service client

--- app/src/main/java/com/example/codearena/MainActivity.java
+++ app/src/main/java/com/example/codearena/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.codearena;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         recyclerView = findViewById(R.id.my_recycler_view);
 
         recyclerView.setHasFixedSize(true);
@@ -33,6 +72,18 @@ public class MainActivity extends AppCompatActivity {
         List<ContestDetails> myDataset = new ArrayList<>();
         myDataset.add(new ContestDetails("9:30PM", "CookOff", "CodeChef", "2.5 hrs"));
         mAdapter = new MyAdapter(myDataset);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
+
+        RestClient rc = new RestClient();
+        try {
+            rc.Execute(RestClient.RequestMethod.GET, "http://34.69.234.16:3300/live", null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/codearena/RestClient.java
@@ -0,0 +1,119 @@
+package com.example.codearena;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+class RestClient {
+    public enum RequestMethod {
+        GET,
+        POST
+    }
+
+    private int responseCode = 0;
+    private String message, response;
+
+    void Execute(RequestMethod method, String url, ArrayList<NameValuePair> headers, ArrayList<NameValuePair> params) throws Exception {
+        switch (method) {
+            case GET: {
+                //add params
+                String combinedParams = "";
+                if (params != null) {
+                    combinedParams += "?";
+                    for (NameValuePair p : params) {
+                        String paramString = p.getName() + "=" + URLEncoder.encode(p.getValue(), "UTF-8");
+                        if (combinedParams.length() > 1)
+                            combinedParams += "&" + paramString;
+                        else
+                            combinedParams += paramString;
+                    }
+                }
+                HttpGet request = new HttpGet(url + combinedParams);
+                //add headers
+                if (headers != null) {
+                    headers = addCommonHeaderField(headers);
+                    for (NameValuePair h : headers)
+                        request.addHeader(h.getName(), h.getValue());
+                }
+                executeRequest(request, url);
+                break;
+            }
+            case POST: {
+                HttpPost request = new HttpPost(url);
+                //add headers
+                if (headers != null) {
+                    headers = addCommonHeaderField(headers);
+                    for (NameValuePair h : headers)
+                        request.addHeader(h.getName(), h.getValue());
+                }
+                if (params != null)
+                    request.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+                executeRequest(request, url);
+                break;
+            }
+        }
+    }
+
+    private ArrayList<NameValuePair> addCommonHeaderField(ArrayList<NameValuePair> _header) {
+        _header.add(new BasicNameValuePair("Content-Type", "application/x-www-form-urlencoded"));
+        return _header;
+    }
+
+    private void executeRequest(HttpUriRequest request, String url) {
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpResponse httpResponse;
+        //Log.d("Checking","Check Successful");
+        try {
+            httpResponse = client.execute(request);
+            Log.d("Checking","Outside Check Successful");
+            responseCode = httpResponse.getStatusLine().getStatusCode();
+            message = httpResponse.getStatusLine().getReasonPhrase();
+            HttpEntity entity = httpResponse.getEntity();
+
+            if (entity != null) {
+                FileWriter file = new FileWriter("C:\\Users\\riyam\\Desktop\\check.txt");
+                file.write(responseCode+"\n");
+                InputStream instream = entity.getContent();
+                response = convertStreamToString(instream);
+                Log.d("Checking","Check Successful");
+                instream.close();
+            }
+            else
+                Log.d("Checking","Check Successful");
+        } catch (Exception e) {
+            Log.d("Debug", e.getMessage());
+        }
+    }
+
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+        } catch (IOException e) {
+            Log.d("Debug", e.getMessage());
+        }
+        return sb.toString();
+    }
+}
